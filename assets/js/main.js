d12b444ea86c03226a7c32caf23c043241ac04c6
Pea <pea@misfist.com>
2015-09-06 18:41:08 -0400

    Changed to using online quotes with jsonp.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,27 +2,52 @@ $(document).ready(function() {
 
     console.log('main.js');
 
-    $('#quote').find('blockquote').hide();
-
     $('#quote-button').click(function() {
 
         console.log('clicked');
 
         $.ajax({
-            url: 'data/quotes.json',
+            url: "http://api.forismatic.com/api/1.0/",
+            jsonp: "jsonp",
+            dataType: "jsonp",
+            data: {
+                method: "getQuote",
+                lang: "en",
+                format: "jsonp"
+            }
         })
-        .success(function(data, x) {
-            var quotes = data[0].quotes;
-            var length = quotes.length;
-            var random = Math.floor( Math.random() * length );
+        .success(function( data, x ) {
+            console.log( 'success', data );
 
-            $('#quote').find('blockquote').show();
-            $('#quote').find('blockquote').html(quotes[random]);
+            // var quotes = data[0].quotes;
+            // var length = quotes.length;
+            // var random = Math.floor( Math.random() * length );
 
-            console.log('random', random);
+            $('#quote blockquote').remove();
+
+            var mainEl = $('#quote');
+            var blockquoteEl = $('<blockquote>');
+            var quoteEl = $( '<p class="quote-text">' );
+            var citeEl = $( '<cite class="quote-author">' );
+            quoteEl.append( data.quoteText );
+
+            if ( data.quoteAuthor ) {
+                citeEl.append( data.quoteAuthor );
+            }
+
+            blockquoteEl.append( quoteEl );
+
+            if ( data.quoteAuthor ) {
+                blockquoteEl.append( citeEl );
+            }
+            
+            mainEl.append( blockquoteEl );
+            
+        })
+        .fail(function( data ) {
+            console.log( 'fail', data );
         });
 
     });
 
 });
-
